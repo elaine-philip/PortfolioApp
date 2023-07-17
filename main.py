999da644f2a2bc4a4a1768b87a2d2f52be05db09
elainephilip <2elainemaria@gmail.com>
2023-07-17 19:20:57 -0400

    Add descp, images, & links

--- main.py
+++ main.py
@@ -19,14 +19,22 @@ content = "Below you can find some of the apps I have built " \
           "in Python. Feel free to contact me!"
 st.write(content)
 
-col3, col4  = st.columns(2)
+col3, empty_col, col4  = st.columns([1.5, 0.5, 1.5])
+#([ration for cols])
 
 df = pandas.read_csv("data.csv", sep=';')
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[Source Code]({row['url']})")
+        #[text](github link to projects)
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[Source Code](http://pythonhow.com)")
